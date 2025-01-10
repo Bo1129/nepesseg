cab7483f9d368a3dbcf73c962619c906f765bd75
Bo1129 <bohmb.22d@acsjszki.hu>
2025-01-10 08:13:50 +0100

    2

---
+++ nepesseg/Orszag.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nepesseg
+{
+    internal class Orszag
+    {
+        public string Orszagnev { get; private set; }
+        public int Terulet { get; private set; }
+        public int Nepesseg { get; private set; }
+        public string Fovaros { get; private set; }
+        public int FovarosNepesseg { get; private set; }
+
+        public Orszag(string sor)
+        {
+            string[] s = sor.Split(';');
+            Orszagnev = s[0];
+            Terulet = int.Parse(s[1]);
+            Nepesseg = int.Parse(s[2]);
+            Fovaros = s[3];
+            FovarosNepesseg = int.Parse(s[4]);
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
+    }
+}
