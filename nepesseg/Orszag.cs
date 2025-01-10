468c836e0ef1081bd192e02027172657e3cc6162
Bo1129 <bohmb.22d@acsjszki.hu>
2025-01-10 08:21:31 +0100

    3

--- nepesseg/Orszag.cs
+++ nepesseg/Orszag.cs
@@ -9,19 +9,19 @@ namespace nepesseg
     internal class Orszag
     {
         public string Orszagnev { get; private set; }
-        public int Terulet { get; private set; }
-        public int Nepesseg { get; private set; }
+        public string Terulet { get; private set; }
+        public string Nepesseg { get; private set; }
         public string Fovaros { get; private set; }
-        public int FovarosNepesseg { get; private set; }
+        public string FovarosNepesseg { get; private set; }
 
         public Orszag(string sor)
         {
             string[] s = sor.Split(';');
             Orszagnev = s[0];
-            Terulet = int.Parse(s[1]);
-            Nepesseg = int.Parse(s[2]);
+            Terulet = s[1];
+            Nepesseg = s[2];
             Fovaros = s[3];
-            FovarosNepesseg = int.Parse(s[4]);
+            FovarosNepesseg = s[4];
         }
 
         public override string ToString()
--- nepesseg/Program.cs
+++ nepesseg/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<Orszag> orszagok = new();
+
+            foreach (var sor in File.ReadAllLines("adatok-utf8.txt").Skip(1))
+            {
+                orszagok.Add(new Orszag(sor));
+
+            }
+            //3. feladat
+            Console.WriteLine($"3.feladat: A beolvasott országok száma: {orszagok.Count}");
+                
+            
         }
     }
 }
